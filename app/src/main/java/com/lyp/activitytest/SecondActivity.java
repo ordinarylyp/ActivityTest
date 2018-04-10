0504351fb68d38595bb6a56070c9f37a4a6514f5
ordinarylyp <2628179064@qq.com>
2018-04-10 20:01:05 +0800

    myapplication

--- app/src/main/java/com/lyp/activitytest/SecondActivity.java
+++ app/src/main/java/com/lyp/activitytest/SecondActivity.java
@@ -23,4 +23,12 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return","Hello FirstActivity");
+        setResult(RESULT_OK,intent);
+        finish();
+    }
 }
